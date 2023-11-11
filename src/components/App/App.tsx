f7155c1cd3d910dfa29b6fe0dd3dae7e95c12811
Alex Barbero <barrufat@hotmail.com>
2023-11-11 02:45:29 +0100

    Add NormiesPage and pagination

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,11 +1,21 @@
+import { Navigate, Route, Routes } from "react-router-dom";
+import Navigation from "../Navigation/Navigation";
 import getImage from "../getImage/getImage";
 import AppStyled from "./AppStyled";
+import NormiesPage from "../../pages/NormiesPage/NormiesPage";
 
 const App = () => {
   return (
-    <AppStyled urlImg={getImage()}>
-      <h1>Normie Wars</h1>
-    </AppStyled>
+    <>
+      <Navigation />
+      <AppStyled urlImg={getImage()}>
+        <Routes>
+          <Route path="/" element={<Navigate to="/home" />} />
+          <Route path="/home" element={<NormiesPage />} />
+          <Route path="/add" element={<Navigate to="/home" />} />
+        </Routes>
+      </AppStyled>
+    </>
   );
 };
 
---
+++ src/pages/NormiesPage/NormiesPage.tsx
@@ -0,0 +1,5 @@
+const NormiesPage = () => {
+  return <h2>Take a look at the Born Warriors</h2>;
+};
+
+export default NormiesPage;
