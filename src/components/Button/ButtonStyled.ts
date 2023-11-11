31f1b2ea9cbb8a5cb60dce391ea3cc9f6b9a96d4
Alex Barbero <barrufat@hotmail.com>
2023-11-11 14:00:02 +0100

    Add ButtonStyled

---
+++ src/components/Button/ButtonStyled.ts
@@ -0,0 +1,14 @@
+import styled from "styled-components";
+
+const ButtonStyled = styled.button`
+  border: none;
+  border-radius: 15px;
+  flex-shrink: 0;
+  background-color: #4c4441;
+  filter: drop-shadow(0px 2px 2px rgba(0, 0, 0, 0.1));
+  padding: 10px;
+  cursor: pointer;
+  color: #fff;
+`;
+
+export default ButtonStyled;
