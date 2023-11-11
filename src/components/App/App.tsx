1a9007a150b553ba0a2a1c34015a5256bbc4cf30
Alex Barbero <barrufat@hotmail.com>
2023-11-11 04:57:26 +0100

    Add NormieCard test and refactor styles

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,14 +1,15 @@
 import { Navigate, Route, Routes } from "react-router-dom";
 import Navigation from "../Navigation/Navigation";
 import getImage from "../getImage/getImage";
-import AppStyled from "./AppStyled";
+import { BackgroundStyled, AppStyled } from "./AppStyled";
 import NormiesPage from "../../pages/NormiesPage/NormiesPage";
 
 const App = () => {
   return (
     <>
       <Navigation />
-      <AppStyled urlImg={getImage()}>
+      <BackgroundStyled url={getImage()} />
+      <AppStyled>
         <Routes>
           <Route path="/" element={<Navigate to="/home" />} />
           <Route path="/home" element={<NormiesPage />} />
--- src/components/App/AppStyled.ts
+++ src/components/App/AppStyled.ts
@@ -1,14 +1,21 @@
 import styled from "styled-components";
 
-const AppStyled = styled.main<{ urlImg: string }>`
+export const AppStyled = styled.main`
+  z-index: 1;
+  position: relative;
   width: 100%;
   height: 100vh;
   display: flex;
   flex-direction: column;
   align-items: center;
   padding: 100px 30px;
-  background-image: url(${(props) => props.urlImg});
-  background-size: cover;
 `;
 
-export default AppStyled;
+export const BackgroundStyled = styled.div<{ url: string }>`
+  width: 100%;
+  height: 100vh;
+  position: fixed;
+  z-index: 0;
+  background-image: url(${(props) => props.url});
+  background-size: cover;
+`;
