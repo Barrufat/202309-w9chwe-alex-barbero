4d5f2e7b2f781bdb705724bd85c47052c7bc1bcf
Alex Barbero <barrufat@hotmail.com>
2023-11-12 02:25:57 +0100

    Add loveNormie and killNormie tests

--- src/store/feature/normies/normiesReducer.test.tsx
+++ src/store/feature/normies/normiesReducer.test.tsx
@@ -1,5 +1,14 @@
-import normiesMock from "../../../mocks/normiesMock";
-import { loadNormiesActionCreator, normiesReducer } from "./normiesSlice";
+import {
+  normiesMock,
+  modifiedFriendNormiesMock,
+  modifiedEnemyNormiesMock,
+} from "../../../mocks/normiesMock";
+import {
+  killEnemyActionCreator,
+  loadNormiesActionCreator,
+  loveFriendActionCreator,
+  normiesReducer,
+} from "./normiesSlice";
 import NormieStructure from "./types";
 
 describe("Given a normiesReducter", () => {
@@ -19,6 +28,44 @@ describe("Given a normiesReducter", () => {
       );
 
       expect(loadedNormies).toStrictEqual(expectedLoadedNormies);
+    });
+  });
+
+  describe("When it recives the action to be friends with a Normie of a list", () => {
+    test("It should update the list with that Normie being a friend", () => {
+      const expectedModifiedNormies: { normies: NormieStructure[] } = {
+        normies: modifiedFriendNormiesMock,
+      };
+
+      const previousState: { normies: NormieStructure[] } = {
+        normies: normiesMock,
+      };
+
+      const modifiedNormies = normiesReducer(
+        previousState,
+        loveFriendActionCreator(1),
+      );
+
+      expect(modifiedNormies).toEqual(expectedModifiedNormies);
+    });
+  });
+
+  describe("When it recives the action to be enemy of a Normie of a list", () => {
+    test("It should update the list with that Normie being an enemy", () => {
+      const expectedModifiedNormies: { normies: NormieStructure[] } = {
+        normies: modifiedEnemyNormiesMock,
+      };
+
+      const previousState: { normies: NormieStructure[] } = {
+        normies: normiesMock,
+      };
+
+      const modifiedNormies = normiesReducer(
+        previousState,
+        killEnemyActionCreator(2),
+      );
+
+      expect(modifiedNormies).toEqual(expectedModifiedNormies);
     });
   });
 });
