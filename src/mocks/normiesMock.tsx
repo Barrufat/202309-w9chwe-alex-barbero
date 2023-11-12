0fa4aa040b042664a84b1fafdc624ef8024c07f5
Alex Barbero <barrufat@hotmail.com>
2023-11-12 17:39:14 +0100

    Fix normiesMock bug

--- src/mocks/normiesMock.tsx
+++ src/mocks/normiesMock.tsx
@@ -9,12 +9,8 @@ export const normiesMock: NormieStructure[] = [
     description:
       "Came from the frozen lands of the Antartida. Her heart is cold like the winter and she's ready to ignore you any time you show that you are happy or you try to make a joke.",
     age: 23,
-    images: [
-      "https://th.bing.com/th/id/OIG.ZrszJetpUzWoHixF3AOq?w=270&h=270&c=6&r=0&o=5&pid=ImgGn",
-      "https://th.bing.com/th/id/OIG.Qz0jHwoCBqNSWkXpjDj0?pid=ImgGn&w=1024&h=1024&rs=1",
+    image:
       "https://cdn.leonardo.ai/users/85d57301-957e-45b5-8213-e4df2524bcc1/generations/ec38e2c3-8744-4c81-9ca6-fc201ba69c99/Leonardo_Diffusion_sketch_funny_cartoon_blackmetal_girl_0.jpg",
-    ],
-    isFriend: undefined,
   },
   {
     id: 2,
@@ -24,12 +20,8 @@ export const normiesMock: NormieStructure[] = [
     description:
       "Just arrived from the Born neightbourhood in Bcn. Avocados and Brunch-Electronic are his passion, and he's always ready to yell at people on twitter while wearing his gafapastas. ",
     age: 33,
-    images: [
-      "https://coloquialmente.com/sites/default/files/inline-images/gafapasta_0.jpg",
-      "https://th.bing.com/th/id/OIG.dwh2FsusQ5qJvDhaXUnZ?pid=ImgGn",
+    image:
       "https://cdn.leonardo.ai/users/85d57301-957e-45b5-8213-e4df2524bcc1/generations/529d5967-cf13-49f4-9fa0-75eb56f6ca42/Leonardo_Diffusion_studio_ghibli_sketch_colorful_happy_arrogan_3.jpg",
-    ],
-    isFriend: undefined,
   },
   {
     id: 3,
@@ -39,12 +31,8 @@ export const normiesMock: NormieStructure[] = [
     description:
       "Always running away from baths and cologne. He will have you listening to his stories about how dangerous he is, but will not tell you about all the money his papa sends to him every weekend. ",
     age: 45,
-    images: [
-      "https://th.bing.com/th/id/OIG.mKlFFhtuZvaIeGPeJP9g?pid=ImgGn&w=1024&h=1024&rs=1",
-      "https://th.bing.com/th/id/OIG._uPtdbeNG4KYKTvRt8j_?pid=ImgGn",
+    image:
       "https://cdn.leonardo.ai/users/85d57301-957e-45b5-8213-e4df2524bcc1/generations/1a6fc1f7-a4b4-4289-9485-e9084d8148e1/Leonardo_Diffusion_studio_ghibli_sketch_cartoon_2d_punk_guy_wi_2.jpg",
-    ],
-    isFriend: undefined,
   },
 ];
 
@@ -57,11 +45,8 @@ export const modifiedFriendNormiesMock: NormieStructure[] = [
     description:
       "Came from the frozen lands of the Antartida. Her heart is cold like the winter and she's ready to ignore you any time you show that you are happy or you try to make a joke.",
     age: 23,
-    images: [
-      "https://th.bing.com/th/id/OIG.ZrszJetpUzWoHixF3AOq?w=270&h=270&c=6&r=0&o=5&pid=ImgGn",
-      "https://th.bing.com/th/id/OIG.Qz0jHwoCBqNSWkXpjDj0?pid=ImgGn&w=1024&h=1024&rs=1",
+    image:
       "https://cdn.leonardo.ai/users/85d57301-957e-45b5-8213-e4df2524bcc1/generations/ec38e2c3-8744-4c81-9ca6-fc201ba69c99/Leonardo_Diffusion_sketch_funny_cartoon_blackmetal_girl_0.jpg",
-    ],
     isFriend: true,
   },
   {
@@ -72,11 +57,8 @@ export const modifiedFriendNormiesMock: NormieStructure[] = [
     description:
       "Just arrived from the Born neightbourhood in Bcn. Avocados and Brunch-Electronic are his passion, and he's always ready to yell at people on twitter while wearing his gafapastas. ",
     age: 33,
-    images: [
-      "https://coloquialmente.com/sites/default/files/inline-images/gafapasta_0.jpg",
-      "https://th.bing.com/th/id/OIG.dwh2FsusQ5qJvDhaXUnZ?pid=ImgGn",
+    image:
       "https://cdn.leonardo.ai/users/85d57301-957e-45b5-8213-e4df2524bcc1/generations/529d5967-cf13-49f4-9fa0-75eb56f6ca42/Leonardo_Diffusion_studio_ghibli_sketch_colorful_happy_arrogan_3.jpg",
-    ],
   },
   {
     id: 3,
@@ -86,11 +68,8 @@ export const modifiedFriendNormiesMock: NormieStructure[] = [
     description:
       "Always running away from baths and cologne. He will have you listening to his stories about how dangerous he is, but will not tell you about all the money his papa sends to him every weekend. ",
     age: 45,
-    images: [
-      "https://th.bing.com/th/id/OIG.mKlFFhtuZvaIeGPeJP9g?pid=ImgGn&w=1024&h=1024&rs=1",
-      "https://th.bing.com/th/id/OIG._uPtdbeNG4KYKTvRt8j_?pid=ImgGn",
+    image:
       "https://cdn.leonardo.ai/users/85d57301-957e-45b5-8213-e4df2524bcc1/generations/1a6fc1f7-a4b4-4289-9485-e9084d8148e1/Leonardo_Diffusion_studio_ghibli_sketch_cartoon_2d_punk_guy_wi_2.jpg",
-    ],
   },
 ];
 
@@ -103,11 +82,8 @@ export const modifiedEnemyNormiesMock: NormieStructure[] = [
     description:
       "Came from the frozen lands of the Antartida. Her heart is cold like the winter and she's ready to ignore you any time you show that you are happy or you try to make a joke.",
     age: 23,
-    images: [
-      "https://th.bing.com/th/id/OIG.ZrszJetpUzWoHixF3AOq?w=270&h=270&c=6&r=0&o=5&pid=ImgGn",
-      "https://th.bing.com/th/id/OIG.Qz0jHwoCBqNSWkXpjDj0?pid=ImgGn&w=1024&h=1024&rs=1",
+    image:
       "https://cdn.leonardo.ai/users/85d57301-957e-45b5-8213-e4df2524bcc1/generations/ec38e2c3-8744-4c81-9ca6-fc201ba69c99/Leonardo_Diffusion_sketch_funny_cartoon_blackmetal_girl_0.jpg",
-    ],
   },
   {
     id: 2,
@@ -117,11 +93,9 @@ export const modifiedEnemyNormiesMock: NormieStructure[] = [
     description:
       "Just arrived from the Born neightbourhood in Bcn. Avocados and Brunch-Electronic are his passion, and he's always ready to yell at people on twitter while wearing his gafapastas. ",
     age: 33,
-    images: [
-      "https://coloquialmente.com/sites/default/files/inline-images/gafapasta_0.jpg",
-      "https://th.bing.com/th/id/OIG.dwh2FsusQ5qJvDhaXUnZ?pid=ImgGn",
+    image:
       "https://cdn.leonardo.ai/users/85d57301-957e-45b5-8213-e4df2524bcc1/generations/529d5967-cf13-49f4-9fa0-75eb56f6ca42/Leonardo_Diffusion_studio_ghibli_sketch_colorful_happy_arrogan_3.jpg",
-    ],
+
     isFriend: false,
   },
   {
@@ -132,10 +106,7 @@ export const modifiedEnemyNormiesMock: NormieStructure[] = [
     description:
       "Always running away from baths and cologne. He will have you listening to his stories about how dangerous he is, but will not tell you about all the money his papa sends to him every weekend. ",
     age: 45,
-    images: [
-      "https://th.bing.com/th/id/OIG.mKlFFhtuZvaIeGPeJP9g?pid=ImgGn&w=1024&h=1024&rs=1",
-      "https://th.bing.com/th/id/OIG._uPtdbeNG4KYKTvRt8j_?pid=ImgGn",
+    image:
       "https://cdn.leonardo.ai/users/85d57301-957e-45b5-8213-e4df2524bcc1/generations/1a6fc1f7-a4b4-4289-9485-e9084d8148e1/Leonardo_Diffusion_studio_ghibli_sketch_cartoon_2d_punk_guy_wi_2.jpg",
-    ],
   },
 ];
