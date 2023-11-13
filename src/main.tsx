c7e8cc8560fa9ef2da755d588a7b62f6ac5fd08f
Alex Barbero <barrufat@hotmail.com>
2023-11-13 02:38:03 +0100

    Fix font type

--- src/main.tsx
+++ src/main.tsx
@@ -5,6 +5,7 @@ import GlobalStyle from "./styles/GlobalStyle";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { store } from "./store";
+import "./styles/index.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
